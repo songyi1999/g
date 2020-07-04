3d0f0d8970ba928c7d88c57abe560754eb66c5c1
songyi1999 <452350370@qq.com>
2020-07-04 21:50:33 +0800

    save

---
+++ src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label for=""> 输入</label>
+        <textarea name="" id="" cols="30" rows="10" v-model="instr"></textarea>
+        <button @click="dochange">提交</button>
+        <textarea name="" id="" cols="30" rows="10" v-model="outstr"></textarea>
+
+    </div>
+</template>
+<script>
+import translate from 'google-translate-open-api'
+export default {
+  data () {
+    return {
+      instr: '',
+      outstr: ''
+    }
+  },
+  methods: {
+    async     dochange () {
+      const result = await translate(this.instr, {
+        tld: 'cn',
+        to: 'zh-CN',
+        browers: true
+      })
+      const data = result.data[0]
+      this.outstr = data
+    }
+  }
+}
+</script>
